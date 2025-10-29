e6b75143787cb7584175c84d3b4d9d82d2711ab5
reza <K.0109266r>
2025-10-29 13:45:45 +0330

    StylingTextField part13

--- app/src/main/java/com/samplecompose/StateReComposition10Activity.kt
+++ app/src/main/java/com/samplecompose/StateReComposition10Activity.kt
@@ -1,7 +1,6 @@
 package com.samplecompose
 
 import android.os.Bundle
-import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,12 +12,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.outlined.Done
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Send
@@ -28,6 +31,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -173,9 +177,72 @@ fun TextField12Jetpack() {
 
             )
 
-            BasicTextField(textNewValue,{textNewValue = it},
-                modifier =Modifier.width(300.dp).background(Color.Red),
-                enabled = true)
+            BasicTextField(
+                textNewValue, { textNewValue = it },
+                modifier = Modifier
+                    .width(300.dp)
+                    .background(Color.Red),
+                enabled = true
+            )
+        }
+
+    }
+}
+
+@Composable
+fun StylingTextField13() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(top = 40.dp),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            var textstate6 by remember { mutableStateOf("") }
+            val myColor6 = Color(0xFFF86B3F)
+
+
+            //t5
+
+            TextField(
+                value = textstate6,
+                onValueChange = { textstate6 = it },
+                modifier = Modifier.padding(top = 20.dp),
+                label = { Text(text = " your name") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email icon",
+                        // tint = myColor5
+                    )
+                },
+                trailingIcon = {
+                    Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedTextColor = myColor6,
+                    unfocusedTextColor = myColor6,
+                    focusedContainerColor = myColor6.copy(alpha = 0.2f),
+                    unfocusedContainerColor = myColor6.copy(alpha = 0.2f),
+                    cursorColor = myColor6,
+                    focusedLeadingIconColor = myColor6,
+                    unfocusedLeadingIconColor = myColor6,
+                    focusedTrailingIconColor = myColor6,
+                    unfocusedTrailingIconColor = myColor6,
+                    focusedLabelColor = myColor6,
+                    unfocusedLabelColor = myColor6,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(25.dp)
+            )
+
         }
 
     }
@@ -185,6 +252,6 @@ fun TextField12Jetpack() {
 @Composable
 fun ButtonTypesPreview() {
     SampleComposeTheme {
-        TextField12Jetpack()
+        StylingTextField13()
     }
 }
