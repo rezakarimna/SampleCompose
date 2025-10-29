5fefc63fac2a2e91db57515dc0412417c4650deb
reza <K.0109266r>
2025-10-29 11:46:46 +0330

    TextField part12

--- app/src/main/java/com/samplecompose/StateReComposition10Activity.kt
+++ app/src/main/java/com/samplecompose/StateReComposition10Activity.kt
@@ -1,30 +1,54 @@
 package com.samplecompose
 
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,11 +100,91 @@ fun StylingText() {
     )
 }
 
+@Composable
+fun TextField12Jetpack() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        val context = LocalContext.current
+        var textNewValue by remember {
+            mutableStateOf("")
+        }
+        var textNewValue2 by remember {
+            mutableStateOf("")
+        }
+        val keyboardControl = LocalSoftwareKeyboardController.current
+        val focusManager = LocalFocusManager.current
+        Column(verticalArrangement = Arrangement.spacedBy(25.dp)) {
+
+            TextField(
+                value = textNewValue, onValueChange = { text ->
+                    textNewValue = text
+                },
+                label = { Text("Enter your name") },
+                maxLines = 2,
+                singleLine = true,
+                modifier = Modifier.width(300.dp),
+                placeholder = { Text("hinttttttttt") },
+                visualTransformation = PasswordVisualTransformation(),
+                leadingIcon = { Icon(Icons.Outlined.Email, "ICon email") },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, textNewValue, Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(Icons.Outlined.Done, "icon action")
+                    }
+                }
+
+            )
+
+            OutlinedTextField(
+                value = textNewValue2, onValueChange = { text ->
+                    textNewValue2 = text
+                },
+                label = { Text("Enter your Email") },
+                maxLines = 2,
+                singleLine = true,
+                modifier = Modifier.width(300.dp),
+                placeholder = { Text("hinttttttttt") },
+                visualTransformation = PasswordVisualTransformation(),
+                leadingIcon = { Icon(Icons.Outlined.Email, "ICon email") },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, textNewValue2, Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(Icons.Outlined.Send, "icon action")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Sentences,
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Send
+
+                ),
+                keyboardActions = KeyboardActions(
+                    onSend = {
+                        keyboardControl?.hide()
+                        Toast.makeText(context, "send button click", Toast.LENGTH_SHORT).show()
+                        focusManager.clearFocus()
+                    }
+                )
+
+
+            )
+
+            BasicTextField(textNewValue,{textNewValue = it},
+                modifier =Modifier.width(300.dp).background(Color.Red),
+                enabled = true)
+        }
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ButtonTypesPreview() {
     SampleComposeTheme {
-        //  ButtonTypes()
-        StylingText()
+        TextField12Jetpack()
     }
 }
